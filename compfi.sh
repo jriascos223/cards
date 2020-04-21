0972482d4f8dba7f8d47d3048e108c79d1eb4e00
Joseph Riascos <josephriascos@gmail.com>
2020-04-20 22:20:08 -0400

    fixed the zip files, now Windows (OS) should work

--- compfi.sh
+++ compfi.sh
@@ -6,13 +6,13 @@ if [ $OSTYPE == "msys" ]
 then
     unzip -nq openjfx-11.0.2_windows-x64_bin-sdk
 else
-    unzip -nq javafx-sdk-11.0.2.zip
+    unzip -nq javafx-sdk-11.0.2
     rm -rf __MACOSX
 fi
 echo "Unzipped JavaFX."
 jfxpath='lib/javafx-sdk-11.0.2/lib'
 cd ..
-javac -d bin -classpath --module-path $jfxpath --add-modules javafx.controls src/tech/jriascos/*/*.java
+javac -d bin -classpath $dir/bin --module-path $jfxpath --add-modules javafx.controls src/tech/jriascos/*/*.java
 echo "Compiled project."
 echo "Running main function found in tech.jriascos.application.Window."
-java -classpath $dir/bin:$dir/lib/ --module-path $jfxpath --add-modules javafx.controls tech.jriascos.application.Window
+java -classpath $dir/bin:$dir/lib --module-path $jfxpath --add-modules javafx.controls tech.jriascos.application.Window
